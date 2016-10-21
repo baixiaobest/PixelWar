77d16e025d4c862ef246c26b984d59eb0f351ace
baixiaobest <baixiaohuang1@gmail.com>
2016-10-21 14:10:53 -0700

    added calculation for body drag, this make aircraft turning more realistic

--- Assets/Scripts/Vehicle/AirplanePhysics.cs
+++ Assets/Scripts/Vehicle/AirplanePhysics.cs
@@ -5,6 +5,7 @@ public class AirplanePhysics : MonoBehaviour {
 	public Transform centerOfMass;
 	public Transform centerOfLift;
 	public Transform centerOfTrust;
+	public Transform centerOfBodyDrag;
 
 	public float stallAngle = 45f;
 	/////////////
@@ -15,10 +16,13 @@ public class AirplanePhysics : MonoBehaviour {
 	// this function is characterized by parameter dragFactor
 	// refer to graphs at https://www.grc.nasa.gov/www/k-12/airplane/inclind.html
 
-	public float smallAngleDragCoefficient = 4f; // drag coefficient under certain degrees
-	public float dragSmallAngle = 5f; 
+	public float smallAngleDragCoefficient = 4f; // constant wing drag coefficient when angle of attack under dragSamllAngle degrees
+	public float dragSmallAngle = 5f;   // within this angle, wing drag coefficient is constant
 	public float dragFactor = 0.5f;  // describes how fast coefficient of drag increases after small angle
 	public float stallDragCoefficient = 6f;
+	// body drag
+	public float initialBodyDragCoefficient = 1f;  // describes how airspeed create drag on aircraft body
+	public float bodyDragFactor=0.1f; // how fast body drag coefficient increases as angle of airflow against aircraft body increases
 
 	////////////
 	/// LIFT ///
@@ -44,14 +48,19 @@ public class AirplanePhysics : MonoBehaviour {
 	private Rigidbody rigid;
 	private float thrust;
 
-	// these control how much of aileron is open, value from -1 to 1;
+	// these control how much of aileron,elevator,rudder and trim are set, value from -1 to 1;
 	private float aileronControl=0;
 	private float elevatorControl=0;
 	private float rudderControl=0;
+	public float elevatorTrim=0;
+
+	// how much elevator can trim
+	private static float MAX_TRIM_PERCENTAGE = 0.3f;
 
 	// debug
 	private float AOA;
 	private float dragCoe;
+	private float bodyDragCoe;
 
 	void Start(){
 		thrust = 0;
@@ -77,7 +86,7 @@ public class AirplanePhysics : MonoBehaviour {
 		Vector3 liftVec = centerOfLift.up * lift;
 		rigid.AddForceAtPosition (liftVec, centerOfLift.position);
 
-		// drag
+		// drag of wing
 		float dragCoefficient;
 		// different drag coefficients for stall and normal state
 		if (Mathf.Abs (angleOfAttack) < stallAngle) {
@@ -86,13 +95,20 @@ public class AirplanePhysics : MonoBehaviour {
 		} else {
 			dragCoefficient = stallDragCoefficient;
 		}
+		float wingDrag = dragCoefficient * rigid.velocity.magnitude * rigid.velocity.magnitude;
+		Vector3 wingDragVec = -rigid.velocity.normalized * wingDrag;
+		rigid.AddForceAtPosition (wingDragVec, centerOfMass.position);
 
-		float drag = dragCoefficient * rigid.velocity.magnitude * rigid.velocity.magnitude;
-		Vector3 dragVec = -rigid.velocity.normalized * drag;
-		rigid.AddForceAtPosition (dragVec, centerOfMass.position);
+		// drag of aircraft body
+		float airflowBodyAngle = Mathf.Min( Vector3.Angle (rigid.velocity, centerOfMass.forward), Vector3.Angle (rigid.velocity, -centerOfMass.forward));
+		float bodyDragCoefficient = initialBodyDragCoefficient + airflowBodyAngle * bodyDragFactor;
+		float bodyDrag = bodyDragCoefficient * rigid.velocity.magnitude * rigid.velocity.magnitude;
+		Vector3 bodyDragVec = -bodyDrag * rigid.velocity.normalized;
+		rigid.AddForceAtPosition (bodyDragVec, centerOfBodyDrag.position);
+
 
 		Vector3 aileronTorque = aileronControl * aileronTorqueCoefficient * airflowSpeed * transform.forward;
-		Vector3 elevatorTorque = elevatorControl * elevatorTorqueCoefficient * airflowSpeed * transform.right;
+		Vector3 elevatorTorque = (elevatorControl + MAX_TRIM_PERCENTAGE * elevatorTrim) * elevatorTorqueCoefficient * airflowSpeed * transform.right;
 		Vector3 rudderTorque = rudderControl * rudderTorqueCoefficient * airflowSpeed * transform.up;
 		rigid.AddTorque (aileronTorque);
 		rigid.AddTorque (elevatorTorque);
@@ -101,28 +117,35 @@ public class AirplanePhysics : MonoBehaviour {
 		// debug
 		AOA = angleOfAttack;
 		dragCoe = dragCoefficient;
+		bodyDragCoe = bodyDragCoefficient;
 		Debug.DrawRay (centerOfTrust.position, 10f * thrustVec/maxThrust, Color.green);
 		Debug.DrawRay (centerOfLift.position, liftVec/500f, Color.green);
-		Debug.DrawRay (centerOfMass.position, dragVec/10f, Color.red);
+		Debug.DrawRay (centerOfMass.position, wingDragVec/10f, Color.red);
+		Debug.DrawRay (centerOfLift.position, rigid.velocity, Color.blue);
+		Debug.DrawRay (centerOfLift.position, centerOfLift.forward * 10f, Color.yellow);
 	}
 
 	public void SetThrust(float thrustPercentage){
-		thrust = maxThrust * thrustPercentage;
+		thrust = maxThrust * Mathf.Clamp (thrustPercentage, 0, 1);
 	}
 
 	// + roll right, - roll left
 	public void SetAileron(float value){
-		aileronControl = value;
+		aileronControl = Mathf.Clamp (value, -1, 1);
 	}
 
 	// + climb up, - pitch down
 	public void SetElevator(float value){
-		elevatorControl = value;
+		elevatorControl = Mathf.Clamp (value, -1, 1);
 	}
 
 	// + turn left, - turn right
 	public void SetRudder(float value){
-		rudderControl = value;
+		rudderControl = Mathf.Clamp (value, -1, 1);
+	}
+
+	public void SetElevatorTrim(float value){
+		elevatorTrim = Mathf.Clamp (value, -1, 1);
 	}
 
 	void OnGUI(){
@@ -130,6 +153,7 @@ public class AirplanePhysics : MonoBehaviour {
 		style.fontSize = 30;
 		GUILayout.Label ("Thrust: " + thrust/maxThrust * 100 + "%", style);
 		GUILayout.Label ("Angle of Attack: "+AOA, style);
-		GUILayout.Label ("Drag Coefficient: " + dragCoe, style);
+		GUILayout.Label ("Wing Drag Coefficient: " + dragCoe, style);
+		GUILayout.Label ("Body Drag Coefficient: " + bodyDragCoe, style);
 	}
 }
