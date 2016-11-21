2d31ec657fd657a93171e18b55f31bc34f86b10b
baixiaobest <baixiaohuang1@gmail.com>
2016-11-21 01:03:56 -0800

    finish tank turret animation

---
+++ Assets/Scripts/Camera/VehicleCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleCamera : MonoBehaviour {
+	public Camera cam;
+	public float sensitivity;
+	private Vector3 initialPos;
+	private Vector3 relativePos;
+
+	void Start () {
+		initialPos = cam.transform.position - transform.position;
+		relativePos = initialPos;
+		GetComponent<ControlRegistration> ().RegisterControl += RegisterControl;
+		GetComponent<ControlRegistration> ().UnregisterControl += UnregisterControl;
+	}
+
+	void RegisterControl(){
+		cam.transform.position = transform.position + initialPos;
+		cam.transform.parent = null;
+		cam.transform.up = Vector3.up;
+		KeyboardEventHandler.MouseMovement += MouseMoved;
+	}
+
+	void UnregisterControl(){
+		cam.transform.parent = transform;
+		KeyboardEventHandler.MouseMovement -= MouseMoved;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.transform.position = transform.position + relativePos;
+	}
+
+	void MouseMoved(float x, float y){
+		cam.transform.RotateAround (transform.position, Vector3.up, x * sensitivity);
+		cam.transform.RotateAround (transform.position, cam.transform.right, -y * sensitivity);
+		relativePos = cam.transform.position - transform.position;
+	}
+}
---
+++ Assets/Scripts/Vehicle/GroundVehicle/TankTurret.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankTurret : MonoBehaviour {
+	public Transform turret;
+	public Transform mainGun;
+	public Transform cam;
+	public float lerp;
+	public float turretTurningSpeed;
+
+	void Update () {
+		Vector3 turretLookat = cam.transform.forward - Vector3.Dot (cam.transform.forward, transform.up) * transform.up;
+		float turretAngle = Vector3.Angle (turretLookat, turret.forward);
+		float sign = Mathf.Sign (Vector3.Dot (Vector3.Cross (turret.forward, turretLookat), transform.up));
+		if(turretAngle > 3f)
+			turret.RotateAround (turret.position, transform.up, sign * turretTurningSpeed * Time.deltaTime);
+
+		if(turretAngle < 60){
+			Vector3 mainGunLookat = cam.transform.forward - Vector3.Dot (cam.transform.forward, turret.right) * turret.right;
+			mainGun.forward = Vector3.Lerp (mainGun.forward, mainGunLookat, lerp * Time.deltaTime);
+		}
+	}
+}
