ef39c94fdf67797e09c7c55d38f0b2e629a933ba
baixiaobest <baixiaohuang1@gmail.com>
2017-02-04 15:46:27 -0800

    updated AI behavior tree

---
+++ Assets/Scripts/CustomTasks/Airplane/EngageAutopilot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class EngageAutopilot : Action {
+	public AutoPilot.AutopilotMode autopilotMode;
+
+	private AutoPilot autopilot;
+	// Use this for initialization
+	public override void OnStart () {
+		autopilot = GetComponent<AutoPilot> ();
+	}
+	
+	// Update is called once per frame
+	public override TaskStatus OnUpdate () {
+		autopilot.EngageAutopilot (autopilotMode);
+		return TaskStatus.Success;
+	}
+}
---
+++ Assets/Scripts/CustomTasks/Airplane/SetAirplaneThrottle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class SetAirplaneThrottle : Action {
+	public float throttle;
+
+	private AirplanePhysics planePhy;
+
+	public override void OnAwake () {
+		planePhy = GetComponent<AirplanePhysics> ();
+	}
+
+	public override TaskStatus OnUpdate () {
+		planePhy.SetThrottle (throttle);
+		return TaskStatus.Success;
+	}
+}
